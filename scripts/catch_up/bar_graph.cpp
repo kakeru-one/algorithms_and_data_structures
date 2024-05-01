877c35b251220b2c562accd4a23fe9c5a51d03ec
ochi-sho-private-study <ochisyoji1118@gmail.com>
2024-05-01 19:12:17 +0900

    APG4bの1-11までやったので、コードを追加する

---
+++ scripts/catch_up/bar_graph.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int A, B;
+  cin >> A >> B;
+
+  // ここにプログラムを追記
+  cout << "A:";
+  int index = 0;
+  while (index < A) {
+    cout << "]";
+    index++;
+  }
+  cout << endl;
+  
+  cout << "B:";
+  index = 0;
+  while (index < B) {
+    cout << "]";
+    index++;
+  }
+  cout << endl;
+}
---
+++ scripts/catch_up/calculator.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N, A;
+  cin >> N >> A;
+
+  // ここにプログラムを追記
+  string op;
+  int B;
+  for (int i = 0; i < N; i++) {
+    cin >> op >> B;
+    if (op == "+") {
+      A += B;
+    } else if (op == "-") {
+      A -= B;
+    } else if (op == "*") {
+      A *= B;
+    } else if (B == 0) {
+      cout << "error" << endl;
+      break;
+    } else if (op == "/") {
+      A /= B;
+    }
+    cout << i + 1 << ":";
+    cout << A << endl;
+  }
+}
