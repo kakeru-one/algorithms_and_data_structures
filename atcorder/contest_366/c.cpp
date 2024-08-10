c7f0247376b5b9e20fef567d0f63ddc665f4c0b4
ochi-sho-private-study <ochisyoji1118@gmail.com>
2024-08-11 02:35:57 +0900

    for文の数を減らす

--- atcorder/contest_366/c.cpp
+++ atcorder/contest_366/c.cpp
@@ -9,6 +9,7 @@ int main() {
   // クエリを格納する。
   // e.g [[1, 3], [1, 1]]
   vector<vector<int> > queries(Q);
+  vector<int> ball_boxes;
 
   for (int i = 0; i < Q; i++) {
     string line;
@@ -19,24 +20,19 @@ int main() {
     while (ss >> num) {
       queries[i].push_back(num); // 空白区切りで数字を格納
     }
-  }
-
-  vector<int> ball_boxes;
-
-  for (int i = 0; i < Q; i++) {
     if (queries[i][0] == 1) {
-        // queries[i][1]をball_boxesの末尾に入れる
-        ball_boxes.push_back(queries[i][1]);
+      // queries[i][1]をball_boxesの末尾に入れる
+      ball_boxes.push_back(queries[i][1]);
     } else if (queries[i][0] == 2) {
-        // queries[i][1]を一つball_boxesから削除する
-        auto it = find(ball_boxes.begin(), ball_boxes.end(), queries[i][1]);
-        if (it != ball_boxes.end()) {
-            ball_boxes.erase(it);
-        }
+      // queries[i][1]を一つball_boxesから削除する
+      auto it = find(ball_boxes.begin(), ball_boxes.end(), queries[i][1]);
+      if (it != ball_boxes.end()) {
+          ball_boxes.erase(it);
+      }
     } else if (queries[i][0] == 3) {
-        // ball_boxesの要素の種類の数を全て標準出力する
-        set<int> unique_balls(ball_boxes.begin(), ball_boxes.end());
-        cout << unique_balls.size() << endl;
+      // ball_boxesの要素の種類の数を全て標準出力する
+      set<int> unique_balls(ball_boxes.begin(), ball_boxes.end());
+      cout << unique_balls.size() << endl;
     }
   }
 
