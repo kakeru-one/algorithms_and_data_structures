e719274a2b227333101eaf86e6ba6ed81fa89173
ochi-sho-private-study <ochisyoji1118@gmail.com>
2024-08-11 02:49:34 +0900

    シンプルに書く

--- atcorder/contest_366/c.cpp
+++ atcorder/contest_366/c.cpp
@@ -2,40 +2,27 @@
 using namespace std;
 
 int main() {
-  int Q;
-  cin >> Q;
-  cin.ignore(); // 最初の改行を無視するために一度だけ使用
-
-  // クエリを格納する。
-  // e.g [[1, 3], [1, 1]]
-  vector<vector<int> > queries(Q);
-  vector<int> ball_boxes;
-
-  for (int i = 0; i < Q; i++) {
-    string line;
-    getline(cin, line); // 一行を読み込む
-
-    stringstream ss(line);
-    int first, second;
-    ss >> first >> second; // 2つの数値を取得
-    queries[i].push_back(first);
-    queries[i].push_back(second);
-
-    if (queries[i][0] == 1) {
-      // queries[i][1]をball_boxesの末尾に入れる
-      ball_boxes.push_back(queries[i][1]);
-    } else if (queries[i][0] == 2) {
-      // queries[i][1]を一つball_boxesから削除する
-      auto it = find(ball_boxes.begin(), ball_boxes.end(), queries[i][1]);
-      if (it != ball_boxes.end()) {
-          ball_boxes.erase(it);
-      }
-    } else if (queries[i][0] == 3) {
-      // ball_boxesの要素の種類の数を全て標準出力する
-      set<int> unique_balls(ball_boxes.begin(), ball_boxes.end());
-      cout << unique_balls.size() << endl;
+    int q;
+    cin >> q;
+    vector<int> cnt(1000000); // cnt[i]=(現時点で (i + 1) が書かれたボールが袋の中に何個あるか//
+    int ans = 0; // 現時点での袋の中のボールに書いてある数の種類数
+    for (int i = 0; i < q; ++i) {
+        int t;
+        cin >> t;
+        if (t == 1) {
+            int x;
+            cin >> x;
+            x--;
+            cnt[x]++;
+            if (cnt[x] == 1) ans++; // このときx が書かれたボールが追加された1個のみである
+        } else if (t == 2) {
+            int x;
+            cin >> x;
+            x--;
+            cnt[x]--;
+            if (cnt[x] == 0) ans--; // このとき x が書かれたボールが袋の中にはもうない
+        } else {
+            cout << ans << "\n";
+        }
     }
-  }
-
-  return 0;
 }
